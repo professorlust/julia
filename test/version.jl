7e72061021f547494d6a82e6c026507c02253fbe
Stefan Karpinski <stefan@karpinski.org>
2018-11-21 16:23:25 -0500

    check_new_version: allow unsorted existing versions

--- test/version.jl
+++ test/version.jl
@@ -210,7 +210,7 @@ end
 # check_new_version
 import Base.check_new_version
 @test check_new_version([v"1", v"2"], v"3") === nothing
-@test_throws AssertionError check_new_version([v"2", v"1"], v"3")
+@test check_new_version([v"2", v"1"], v"3") === nothing
 @test_throws ErrorException check_new_version([v"1", v"2"], v"2")
 @test check_new_version(VersionNumber[], v"0") === nothing
 @test check_new_version(VersionNumber[], v"0.0.1") === nothing
